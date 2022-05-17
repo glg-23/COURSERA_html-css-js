4fc5b0e36e0225bcaa4d40f43e9c7d0901301733
glg-23 <gianluigi.dantimi@gmail.com>
2022-05-17 17:31:06 +0200

    module 5 Assignment

--- Module_5/js/script.js
+++ Module_5/js/script.js
@@ -63,6 +63,10 @@ var switchMenuToActive = function () {
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
+
+// START
+
+
 // TODO: STEP 0: Look over the code from
 // *** start ***
 // to
@@ -83,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -104,6 +108,9 @@ function buildAndShowHomeHTML (categories) {
       // var chosenCategoryShortName = ....
 
 
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+
+
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -118,10 +125,17 @@ function buildAndShowHomeHTML (categories) {
       // var homeHtmlToInsertIntoMainPage = ....
 
 
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+
+
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+
+
+      insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
+
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
@@ -341,3 +355,6 @@ function insertItemPortionName(html,
 global.$dc = dc;
 
 })(window);
+
+
+/* Gianluigi D'Antimi */
